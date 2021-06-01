bf4c091483dbbbe738951ad7659e574288de80a2
Unknown <99053654@mydavinci.nl>
2021-06-01 13:46:46 +0200

    added the namevalues to the class

---
+++ POKEBATTLE/Pokemon.php
@@ -0,0 +1,13 @@
+<?php 
+	class Pokemon {
+		public $name;
+		public $energyType;
+		public $hitPoints;
+		public $health;
+		public $attacks;
+		public $weakness;
+		public $resitance;
+		public $attackAttacks;
+	}	
+
+?>
