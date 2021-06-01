d87b21515925fbc9cd22241bd2285cc2c27b8fec
Unknown <99053654@mydavinci.nl>
2021-06-01 13:53:55 +0200

    testing it out with the pokemon class

--- POKEBATTLE/Pokemon.php
+++ POKEBATTLE/Pokemon.php
@@ -8,6 +8,21 @@
 		public $weakness;
 		public $resitance;
 		public $attackAttacks;
+
+		public function __construct($name, $energyType, $hitPoints, $health, $attacks, $weakness, $resitance, $attackAttacks) {
+        	$this->name = $name;
+        	$this->energyType = $energyType;
+        	$this->hitPoints = $hitPoints;
+        	$this->health = $health;
+        	$this->attacks = $attacks;
+        	$this->weakness = $weakness;
+        	$this->resitance = $resitance;
+        	$this->attackAttacks = $attackAttacks;
+    	}
+
+		public function __toString() {
+	        return json_encode($this);
+	    }
 	}	
 
 ?>
--- POKEBATTLE/index.php
+++ POKEBATTLE/index.php
@@ -1,6 +1,6 @@
 <?php 
 	require 'Pokemon.php';
 
-	$pokemon = new Pokemon;
+	$pokemon = new Pokemon('Pikachu', 'lightning', 40, 100, 1, 2, 4, 'attack');
 	echo $pokemon;
 ?>
