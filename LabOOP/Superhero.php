122e9bf1106dd0136b0f4fef65c700fd177b74f6
Unknown <99053654@mydavinci.nl>
2021-06-01 10:14:03 +0200

    adding new functions in classes

--- LabOOP/Superhero.php
+++ LabOOP/Superhero.php
@@ -5,6 +5,18 @@ class Superhero {
 	public $gender;
 	public $team;
 	public $oneline;
+
+	public function __toString() {
+        return json_encode($this);
+    }
 }
+
+public function __construct($name, $gender, $team, $oneliner)
+    {
+        $this->name = $name;
+        $this->gender = $gender;
+        $this->team = $team;
+        $this->oneliner = $oneliner;
+    }
 
 ?>
