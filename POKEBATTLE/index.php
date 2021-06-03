0f1daae8a93493db71096f44475dfc499b42d49f
Unknown <99053654@mydavinci.nl>
2021-06-03 11:02:41 +0200

    printed the attack on the screen need to fix the health they have before and after

--- POKEBATTLE/index.php
+++ POKEBATTLE/index.php
@@ -7,8 +7,14 @@
 	require 'Resistance.php';
 	require 'Weakness.php';
 
+	// Pikachu valt Charmeleon aan met een Electric Ring attack
+	// Charmeleon valt Pikachu aan met een Flare attack
+	// Print voor en na de aanval de health uit van de pokemon die wordt aangevallen
+	$Pikachu = new Pikachu('Pikachu');
 	$Charmeleon = new Charmeleon('Charmeleon');
-	echo $Charmeleon;
+	echo $Pikachu->name . ' valt ' . $Charmeleon->name. ' aan met een ' . $Pikachu->attacks[0] . ' attack ' . "<br>";
+	echo $Charmeleon->name . ' valt ' . $Pikachu->name . ' aan met een ' . $Charmeleon->attacks[1] . ' attack';
+	// echo $Charmeleon;
 
 	// $Energytype = new Energytype('water', 40);
 	// echo $Energytype;
