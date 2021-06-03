8ae1a2be3f49d7fc925f0b6cb1b7f19d52d5f447
Unknown <99053654@mydavinci.nl>
2021-06-03 10:04:17 +0200

    changed th constructor for the chartmeleon

--- POKEBATTLE/Charmeleon.php
+++ POKEBATTLE/Charmeleon.php
@@ -1,22 +1,16 @@
 <?php
 	class Charmeleon extends Pokemon {
-		public $name = 'Charmeleon';
-		public $energyType = 'Fire';
-		public $hitPoints = 60;
-		public $health = 100;
-		public $attacks = 2;
-		public $weakness = 2;
-		public $resitance = 10;
 
-		public function __construct($name, $energyType, $hitPoints, $health, $attacks, $weakness, $resitance) {
-        	$this->name = $name;
-        	$this->energyType = $energyType;
-        	$this->hitPoints = $hitPoints;
-        	$this->health = $health;
-        	$this->attacks = $attacks;
-        	$this->weakness = $weakness;
-        	$this->resitance = $resitance;
-    	}
+		public function __construct($name) {
+			$energyType = 'Fire';
+			$hitPoints = 60;
+			$health = 100;
+			$attacks = [];
+			$weakness = 2;
+			$resitance = 10;
+			parent:: __construct($name, $energyType, $hitPoints, $health, $attacks, $weakness, $resitance);
+
+	   }
 
 		public function __toString() {
 	        return json_encode($this);
