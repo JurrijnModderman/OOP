096ebb737c9e7ebc5076038fdc1875b31281b8bc
Unknown <99053654@mydavinci.nl>
2021-06-04 11:21:19 +0200

    added the constructor and the tostring function to weakness and resistance

--- POKEBATTLE/Resistance.php
+++ POKEBATTLE/Resistance.php
@@ -1,5 +1,13 @@
 <?php
 	class Resistance {
 		public $resistanceValue;
+
+		public function __construct($resistanceValue) {
+			$this->resistanceValue = $resistanceValue;
+		}
+
+		public function __toString() {
+			return json_encode($this);
+		}
 	}
 ?>
--- POKEBATTLE/Weakness.php
+++ POKEBATTLE/Weakness.php
@@ -2,6 +2,14 @@
 	class Weakness {
 		public $weaknessMultiplier;
 
+		public function __construct() {
+			$this->weaknessMultipier = $weaknessMultiplier;
+		}
+
+
+		public function __toString() {
+			return json_encode($this);
+		}
 
 	}
 ?>
