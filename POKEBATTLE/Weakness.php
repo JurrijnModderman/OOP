9321cb916f46b8fb581df7b551804518b6ee62c5
Unknown <99053654@mydavinci.nl>
2021-06-08 13:29:49 +0200

    changed the weakness and resistance values in the pokemon classes

--- POKEBATTLE/Weakness.php
+++ POKEBATTLE/Weakness.php
@@ -1,9 +1,11 @@
 <?php
 	class Weakness {
 		public $weaknessMultiplier;
+		public $weaknessEnergyType;
 
-		public function __construct() {
+		public function __construct($weaknessMultiplier, $weaknessEnergyType) {
 			$this->weaknessMultipier = $weaknessMultiplier;
+			$this->weaknessEnergyType = $weaknessEnergyType;
 		}
 
 
