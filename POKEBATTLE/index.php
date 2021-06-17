7a52784a4fb9b3a5b44f6bf6f1828d66e339a384
Unknown <99053654@mydavinci.nl>
2021-06-17 10:24:57 +0200

    deleted the last things

--- POKEBATTLE/index.php
+++ POKEBATTLE/index.php
@@ -14,8 +14,6 @@
 	// Print voor en na de aanval de health uit van de pokemon die wordt aangevallen
 	$pikachu = new Pikachu(name :'Pikachu');
 	$charmeleon = new Charmeleon(name :'Charmeleon');
-	;
-	$charmeleon->attack(target: $pikachu, attackNumber: 0);
 
 	echo $pikachu->getName() . ' health : ' . $charmeleon->getHealth() . ' / ' . $pikachu->getHitPoints();
 	echo '<br>' . $pikachu->getName() . ' Attacks ' . $charmeleon->getName() . ' with a ' . $pikachu->attack(target: $charmeleon, attackNumber: 0) . ' attack ' . '<br>';
